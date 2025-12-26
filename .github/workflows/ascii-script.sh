05dd57d93bc80a06054ca545ef58dcd6ccba33f2
Shree <srinu.galla@gmail.com>
2025-12-26 19:22:01 +0000

    path & code updated

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 sudo apt-get install cowsay -y
+cowsay "Hello from ASCII script!"
 cowsay -f dragon "Run for cover, I am a DRAGON...RAWR" >> dragon.txt
 grep -i "dragon". dragon.txt
 cat dragon.txt
