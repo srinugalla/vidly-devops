e06c34c6e51e877ec573f10c7523cfdbf5377b59
Shree <srinu.galla@gmail.com>
2025-12-29 11:42:33 +0000

    Delete .github/workflows directory
    deleting github actions/ workflow directory

--- .github/workflows/ascii-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get install cowsay -y
-cowsay "Hello from ASCII script!"
-cowsay -f dragon "Run for cover, I am a DRAGON...RAWR" >> dragon.txt
-grep -i "dragon". dragon.txt
-cat dragon.txt
-ls -ltr
